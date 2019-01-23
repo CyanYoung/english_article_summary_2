2f6b2d88e9fcd3663216604c24d5190d777fe566
yangjc <bupt_yjc@163.com>
2019-01-23 17:43:50 +0800

    simplify load

--- summary.py
+++ summary.py
@@ -13,7 +13,7 @@ from represent import sent2ind
 
 from nn_arch import PtrEncode, PtrDecode, PtrCore
 
-from util import trunc, map_item
+from util import map_item
 
 
 plt.rcParams['axes.unicode_minus'] = False
@@ -28,7 +28,7 @@ def load_model(name, embed_mat, device, mode):
     part = arch(embed_mat).to(device)
     part_dict = part.state_dict()
     for key, val in full_dict.items():
-        key = trunc(key, num=1)
+        key = '.'.join(key.split('.')[1:])
         if key in part_dict:
             part_dict[key] = val
     part.load_state_dict(part_dict)
